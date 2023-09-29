055d40d55048db7a55e22463d6aad118f18a0d67
Davor Petrovikj <dp788@njit.edu>
2023-09-29 17:35:43 -0400

    making first model

--- wellfare/wellfare/settings.py
+++ wellfare/wellfare/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
--- wellfare/wellfare/urls.py
+++ wellfare/wellfare/urls.py
@@ -20,3 +20,11 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
