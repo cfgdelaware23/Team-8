525f0e5019d9987be1145b77fd14dabb1ab38e57
Davor Petrovikj <dp788@njit.edu>
2023-09-29 19:06:44 -0400

    rev

--- wellfare/polls/models.py
+++ wellfare/polls/models.py
@@ -1,4 +1,3 @@
-#models
 from django.db import models
 
 
@@ -11,5 +10,3 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
-
--- wellfare/wellfare/settings.py
+++ wellfare/wellfare/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -122,4 +121,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
