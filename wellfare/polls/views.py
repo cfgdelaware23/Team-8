8bccec3d10e6fa408010682651e41eaf81a85dd9
Davor Petrovikj <dp788@njit.edu>
2023-09-29 18:31:22 -0400

    django db setup

--- wellfare/polls/views.py
+++ wellfare/polls/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, JPMC. This is team 8")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
