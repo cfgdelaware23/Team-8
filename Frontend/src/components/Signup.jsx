803bec5746b0a1fb401f497754ccd5021a61fd64
michaelagary <michaelanicole03@gmail.com>
2023-09-29 20:57:58 -0400

    improvements to signup

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -11,7 +11,11 @@ export default function Signup() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`The name you entered was: ${inputs.name}`)
+    console.log(`Enter your full name: ${inputs.name}`)
+    console.log(`Enter your cellphone number: ${inputs.number}`)
+    console.log(`Are you enrolled in an assistance program? ${inputs.assistance}`)
+    console.log(`Do you live in public housing? ${inputs.housing}`)
+    console.log(`Have you faced food insecurity? ${inputs.insecurity}`)
   }
 
   return (
@@ -24,7 +28,7 @@ export default function Signup() {
         onChange={handleChange}
         required />
       </label><br></br>
-      <label>Enter cellphone number: 
+      <label>Enter your cellphone number: 
         <input 
           type="tel" 
           name="number" 
@@ -38,25 +42,29 @@ export default function Signup() {
            <input
             type="radio"
             name="assistance"
-            value={inputs.assistance} 
+            value="SNAP"
+            checked={inputs.assistance === "SNAP"}
             onChange={handleChange}
             required /> SNAP
             <input
             type="radio"
             name="assistance"
-            value={inputs.assistance} 
+            value="WIC"
+            checked={inputs.assistance === "WIC"}
             onChange={handleChange}
             required /> WIC
             <input
             type="radio"
             name="assistance"
-            value={inputs.assistance} 
+            value="Both"
+            checked={inputs.assistance === "Both"} 
             onChange={handleChange}
             required /> Both
             <input
             type="radio"
             name="assistance"
-            value={inputs.assistance} 
+            value="None"
+            checked={inputs.assistance === "None"}
             onChange={handleChange}
             required /> None
         </label>
@@ -66,15 +74,35 @@ export default function Signup() {
            <input
             type="radio"
             name="housing"
-            value={inputs.housing} 
+            value="Yes"
+            checked={inputs.housing === "Yes"}
             onChange={handleChange}
             required /> Yes
             <input
             type="radio"
             name="housing"
-            value={inputs.housing} 
+            value="No"
+            checked={inputs.housing === "No"}
             onChange={handleChange}
             required /> No
+            </label>
+        <br></br>
+        <label>
+        Have you faced food insecurity? {/* radio buttons */}
+           <input
+            type="radio"
+            name="insecurity"
+            value="Yes"
+            checked={inputs.insecurity === "Yes"}
+            onChange={handleChange}
+            /> Yes
+            <input
+            type="radio"
+            name="insecurity"
+            value="No"
+            checked={inputs.insecurity === "No"}
+            onChange={handleChange}
+            /> No
             </label>
             <br></br>
         <input type="submit" />
