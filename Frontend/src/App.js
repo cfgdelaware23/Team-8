af5e6c704ca8a5c8e71a5074337fd03e0a64658a
michaelagary <michaelanicole03@gmail.com>
2023-09-30 10:45:48 -0400

    cart added to checkout

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,7 +13,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <Scan/>
+      <Checkout/>
     </div>
   );
 }
--- Frontend/src/components/Checkout.jsx
+++ Frontend/src/components/Checkout.jsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Grid from '@mui/material/Grid';
 
 export default function Checkout() {
 
@@ -7,7 +13,59 @@ export default function Checkout() {
             <br></br>
             <img src="https://codeforgood.net/wp-content/uploads/2023/09/Wellfare-Logo-1536x304.png" height="50px"/>
             <h2>Greetings Bill!</h2>
-            <p>Bob recieves <b>40%</b> off on each purchase. Today he spent <b>$26.79</b> and saved <b>$8.36</b></p>
+                <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={2}>
+                            <Card sx={{ width: 250 }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://images.everydayhealth.com/images/diet-nutrition/all-about-bananas-nutrition-facts-health-benefits-recipes-and-more-rm-722x406.jpg"
+                                    alt="bananas"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Bananas
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        $5.00
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={2}>
+                            <Card sx={{ width: 250 }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://clv.h-cdn.co/assets/15/22/2560x1728/gallery-1432664914-strawberry-facts1.jpg"
+                                    alt="strawberries"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Strawberries
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        $5.15
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            <p>Bob recieves <b>40%</b> off on each purchase.</p>
+            <p> Today Bob's total is <b>$6.09</b>, he saved <b>$4.06</b>.</p>
+            <p> This gives him <b>6.09</b> reward points</p>
         </div>
     )
 
