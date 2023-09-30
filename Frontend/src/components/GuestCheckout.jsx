371e65b8595457c8a9b6c1a24eae3944403f007e
michaelagary <michaelanicole03@gmail.com>
2023-09-30 10:46:35 -0400

    cart added to guest checkout

--- Frontend/src/components/GuestCheckout.jsx
+++ Frontend/src/components/GuestCheckout.jsx
@@ -7,7 +7,57 @@ export default function GuestCheckout() {
             <br></br>
             <img src="https://codeforgood.net/wp-content/uploads/2023/09/Wellfare-Logo-1536x304.png" height="50px"/>
             <h2>Greetings Bill!</h2>
-            <p>The customer owes <b>$112.74</b></p>
+            <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={2}>
+                            <Card sx={{ width: 250 }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://images.everydayhealth.com/images/diet-nutrition/all-about-bananas-nutrition-facts-health-benefits-recipes-and-more-rm-722x406.jpg"
+                                    alt="bananas"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Bananas
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        $5.00
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <br></br>
+                <br></br>
+                <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+                    <Grid item xs={12}>
+                        <Grid container justifyContent="center" spacing={2}>
+                            <Card sx={{ width: 250 }}>
+                                <CardActionArea>
+                                    <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image="https://clv.h-cdn.co/assets/15/22/2560x1728/gallery-1432664914-strawberry-facts1.jpg"
+                                    alt="strawberries"
+                                    />
+                                    <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        Strawberries
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        $5.15
+                                    </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            <p>This customer's total is <b>$10.15</b></p>
         </div>
     )
 
